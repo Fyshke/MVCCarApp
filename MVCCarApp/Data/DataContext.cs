c9fea9cc23be6a9c5f292cec46c76e54af30c444
Ivan Fišić <ivfisic@vsite.hr>
2023-11-18 00:00:46 +0100

    Add project files.

---
+++ MVCCarApp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCCarApp.Models;
+
+namespace MVCCarApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCCarApp.Models.Car> Car { get; set; } = default!;
+    }
+}
---
+++ MVCCarApp/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCCarApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int ProductionYear { get; set; }
+        [Column(TypeName = "decimal(8,2")]
+        public decimal Price { get; set; }
+        public string Manufacturer { get; set; } = string.Empty;
+    }
+}
